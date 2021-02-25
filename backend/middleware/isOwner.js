3f34aea55633970b0b04d3fe2c2737eb3bed477b
Arthentia <russoelisa@hotmail.fr>
2021-02-25 11:35:56 +0100

    isAdmin

--- backend/middleware/isOwner.js
+++ backend/middleware/isOwner.js
@@ -4,26 +4,29 @@ const Article = require('../models/article');
 const User = require('../models/user')
 
 
-
 module.exports = (req, res, next) => {
     try {
         const token = req.headers.authorization.split(' ')[1];
         const decodedToken = jwt.verify(token, process.env.TOKEN);
         const userId = decodedToken.userId;
+        let isAdminBool = null;
+        User.findOne({
+            where: {
+                id: userId
+            }
+        })
+            .then(user => { isAdminBool = user.isAdmin })
+            .catch(error => res.status(400).json({ error }));
         Article.findOne({
             where: {
                 id: req.params.id
             }
         })
             .then(article => {
-                if (article.userId == userId) {
+                if (article.userId == userId || isAdminBool == true) {
                     next();
-
-
                 } else {
                     throw 'Invalid user ID';
-
-
                 }
             })
             .catch(error => res.status(400).json({ error }));
